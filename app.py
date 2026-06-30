b6e17edec0885c64b28a8da01fe8e65669d32314
nithyasrid <nithyasridevasenan9706@gmail.com>
2026-06-30 11:02:15 +0530

    initial

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask 
+Flask =  (__name__)
+@app.router('/', methods=['GET'])
+def home():
+    return jsonify({"http://4.224.186.213/evaluation-service/depots"})
+
+print(status_code)
+

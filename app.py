90bb49441d4de3648e87e67fbbba1a1e3c303c86
NITHYA SRI D <nithyasridevasenan9706@gmail.com>
2026-08-07 00:42:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask 
+'''from flask import Flask 
 Flask =  (__name__)
 @app.router('/', methods=['GET'])
 def home():
     return jsonify({"http://4.224.186.213/evaluation-service/depots"})
 
 print(status_code)
-
+'''
 '''
 In order to create depot api ;
 create an empty list named as depots
